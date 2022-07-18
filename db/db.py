a3486d886767efd1552910aae00112f1e98e19ec
AquarellFantasia <em224342@gmail.com>
2022-07-19 00:39:46 +0200

    db finished

--- db/db.py
+++ db/db.py
@@ -1,12 +1,13 @@
 import sqlite3
 import os
-from sqlite3 import dbapi2
+import pandas as pd
 
-class Database:
+db_dir = os.getenv('SCRAPPER_DB_PATH')
+db_path = "{}apartments.db".format(db_dir)
+
+class Database:    
     def __init__(self):
-        db_path = os.getenv('SCRAPPER_DB_PATH')
-        path = "{}apartments.db".format(db_path)
-        self.con = sqlite3.connect(path)
+        self.con = sqlite3.connect(db_path)
         self.cursor = self.con.cursor()
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS apartments
                                 (id text PRIMARY KEY,
@@ -42,15 +43,40 @@ class Database:
                                         type, link))
         self.con.commit()
     
-    def readAll(self):
+    def clean_db(self):
+        self.cursor.execute("DELETE FROM apartments")
+        self.con.commit()
+        print("Database cleaned")
+    
+    def to_string(self):
         for row in self.cursor.execute("SELECT * FROM apartments"):
             print(row)
 
-db = Database()
-db.insert("test", "test2", 1.0, 2.0, "test1", 1, "test2",  1, 1, 1, 1, "test3", "test4")
-db.insert("test2", "test2", 1.0, 2.0, "test3", 1, "test4", 1, 1, 1, 1, "test5", "test6")
-db.insert("test2", "test2", 1.0, 2.0, "test11", 1, "test4",1, 1, 1, 1, "test5", "test6")
-db.insert("test3", "test2", 1.0, 2.0, "test10", 1, "test4",1, 1, 1, 1, "test5", "test6")
-db.readAll()
+    def to_csv(self, location):
+        conn = sqlite3.connect(db_path, isolation_level=None,
+                       detect_types=sqlite3.PARSE_COLNAMES)
+        db_df = pd.read_sql_query("SELECT * FROM apartments", conn)
+        db_df.to_csv('{}database.csv'.format(location), index=False)
+        
+        
+
+# db = Database()
+# db.clean_db()
+# db.to_string()
+# db.insert("test", "test2", 1.0, 2.0, "test1", 1, "test2",  1, 1, 1, 1, "test3", "test4")
+# db.insert("test2", "test2", 1.0, 2.0, "test3", 1, "test4", 1, 1, 1, 1, "test5", "test6")
+# db.insert("test2", "test2", 1.0, 2.0, "test11", 1, "test4",1, 1, 1, 1, "test5", "test6")
+# db.insert("test3", "test2", 1.0, 2.0, "test10", 1, "test4",1, 1, 1, 1, "test5", "test6")
+
+# db.to_string()
+
+# db.to_csv(db_dir)
+
+# print('CSV created')
+# df = pd.read_csv('{}database.csv'.format(db_dir))
+# print(df['city'])
+
+
+
 
 
